53f7f55cd9bbabdcf7883d180f874f664f19f81d
niamu01 <niamu01@naver.com>
2022-08-18 12:50:05 +0900

    test case

--- vector_test.cpp
+++ vector_test.cpp
@@ -7,36 +7,9 @@
   #include "vector.hpp"
 #endif
 
-/* todo:
- * iterator != const_iterator
- */
-
 int		main(void)
 {
-  const int size = 5;
-  ft::vector<int> vct(size);
-  ft::vector<int>::reverse_iterator it = vct.rbegin();
-  ft::vector<int>::const_reverse_iterator ite = vct.rbegin();
 
-  for (int i = 0; i < size; ++i)
-    it[i] = (size - i) * 5;
-
-  it = it + 5;
-  it = 1 + it;
-  it = it - 4;
-  std::cout << *(it += 2) << std::endl;
-  std::cout << *(it -= 1) << std::endl;
-
-  *(it -= 2) = 42;
-  *(it += 2) = 21;
-
-  std::cout << "const_ite +=/-=: " << *(ite += 2) << " | " << *(ite -= 2) << std::endl;
-
-  std::cout << "(it == const_it): " << (ite == it) << std::endl;
-  std::cout << "(const_ite - it): " << (ite - it) << std::endl;
-  std::cout << "(ite + 3 == it): " << (ite + 3 == it) << std::endl;
-
-  std::cout << vct.size() << ", " << vct.capacity() << std::endl;
 
 //  const int size = 5;
 //  ft::vector<int> vct(size);
