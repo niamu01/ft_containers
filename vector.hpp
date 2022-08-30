9f5e85eba4b92c24fb6b5250b5efcaf9447a0536
niamu01 <niamu01@naver.com>
2022-08-30 22:39:32 +0900

    vec_test file

--- vector.hpp
+++ vector.hpp
@@ -668,35 +668,36 @@ namespace ft {
 //  lhs.swap(rhs);
 //}
 //
-///* operator */
-//template< class T, class Alloc >
-//bool operator==( const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs ) {
-//  return lhs.size() == rhs.size() && ft::equal(lhs.begin(), lhs.end(), rhs.begin());
-//}
-//
-//template< class T, class Alloc >
-//bool operator!=( const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs ) {
-//  return !(operator==(lhs, rhs));
-//}
-//
-//template< class T, class Alloc >
-//bool operator< ( const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs ) {
-//  return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
-//}
-//
-//template< class T, class Alloc >
-//bool operator> ( const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs ) {
-//  return !ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()) && operator!=(lhs, rhs);
-//}
-//
-//template< class T, class Alloc >
-//bool operator<=( const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs ) {
-//  return !(operator>(lhs, rhs));
-//}
-//
-//template< class T, class Alloc >
-//bool operator>=( const ft::vector<T>& lhs, const ft::vector<T,Alloc>& rhs ) {
-//  return !(operator<(lhs, rhs));
-//}
+
+/* operator */
+template< class T, class Alloc >
+bool operator==( const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs ) {
+  return lhs.size() == rhs.size() && ft::equal(lhs.begin(), lhs.end(), rhs.begin());
+}
+
+template< class T, class Alloc >
+bool operator!=( const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs ) {
+  return !(operator==(lhs, rhs));
+}
+
+template< class T, class Alloc >
+bool operator< ( const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs ) {
+  return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+}
+
+template< class T, class Alloc >
+bool operator> ( const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs ) {
+  return !ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()) && operator!=(lhs, rhs);
+}
+
+template< class T, class Alloc >
+bool operator<=( const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs ) {
+  return !(operator>(lhs, rhs));
+}
+
+template< class T, class Alloc >
+bool operator>=( const ft::vector<T>& lhs, const ft::vector<T,Alloc>& rhs ) {
+  return !(operator<(lhs, rhs));
+}
 
 #endif
--- vector_test.cpp
+++ vector_test.cpp
@@ -20,7 +20,7 @@ const std::string isCapacityOk = (capacity >= size) ? "OK" : "KO";
 // Cannot limit capacity's max value because it's implementation dependent
 
 std::cout << "size: " << size << std::endl;
-std::cout << "capacity: " << isCapacityOk << std::endl;
+std::cout << "capacity: " << isCapacityOk << capacity << std::endl;
 std::cout << "max_size: " << vct.max_size() << std::endl;
 if (print_content)
 {
@@ -65,39 +65,29 @@ void	cmp(const ft::vector<T, Alloc> &lhs, const ft::vector<T, Alloc> &rhs)
 #include <list>
 int		main(void)
 {
-  ft::vector<TESTED_TYPE> vct(10);
+
+  ft::vector<TESTED_TYPE> vct(5);
   ft::vector<TESTED_TYPE> vct2;
-  ft::vector<TESTED_TYPE> vct3;
+  const int cut = 3;
 
   for (unsigned long int i = 0; i < vct.size(); ++i)
-    vct[i] = (vct.size() - i) * 3;
+    vct[i] = (vct.size() - i) * 7;
   printSize(vct);
 
-  vct2.insert(vct2.end(), 42);
-  vct2.insert(vct2.begin(), 2, 21);
+  vct2.insert(vct2.begin(), vct.begin(), vct.begin() + cut);
+  printSize(vct2);
+  vct2.insert(vct2.begin(), vct.begin() + cut, vct.end());
+  printSize(vct2);
+  vct2.insert(vct2.end(), vct.begin(), vct.begin() + cut);
   printSize(vct2);
 
-  vct2.insert(vct2.end() - 2, 42);
+  std::cout << "insert return:" << std::endl;
+
+  std::cout << *vct2.insert(vct2.end(), 42) << std::endl;
+  std::cout << *vct2.insert(vct2.begin() + 5, 84) << std::endl;
+  std::cout << "----------------------------------------" << std::endl;
+
   printSize(vct2);
-
-  std::cout << "a" << std::endl;
-  vct2.insert(vct2.end(), 2, 84);
-  printSize(vct2);
-
-  vct2.resize(4);
-  printSize(vct2);
-
-  vct2.insert(vct2.begin() + 2, vct.begin(), vct.end());
-  vct.clear();
-  printSize(vct2);
-
-  printSize(vct);
-
-  for (int i = 0; i < 5; ++i)
-    vct3.insert(vct3.end(), i);
-  vct3.insert(vct3.begin() + 1, 2, 111);
-  printSize(vct3);
-
 
 //  const int size = 5;
 //  ft::vector<int> vct(size);
