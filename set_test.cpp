bb36649e2995dfa1d9603f1415491c31bc6dba52
niamu01 <niamu01@naver.com>
2022-09-11 08:45:57 +0900

    fix:set

--- set_test.cpp
+++ set_test.cpp
@@ -100,63 +100,53 @@ void	printReverse(ft::set<T1> &st)
   std::cout << "_______________________________________________" << std::endl;
 }
 
-
-#define T1 int
-typedef ft::set<T1>::iterator ft_iterator;
-typedef ft::set<T1>::const_iterator ft_const_iterator;
+#define T1 std::string
+typedef ft::set<T1>::iterator iterator;
 
 static int iter = 0;
 
-template <typename SET>
-void	ft_bound(SET &st, const T1 &param)
+template <typename SET, typename U>
+void	ft_insert(SET &st, U param)
 {
-  ft_iterator ite = st.end(), it[2];
-  ft::pair<ft_iterator, ft_iterator> ft_range;
+	_pair<iterator, bool> tmp;
 
-  std::cout << "\t-- [" << iter++ << "] --" << std::endl;
-  std::cout << "with key [" << param << "]:" << std::endl;
-  it[0] = st.lower_bound(param); it[1] = st.upper_bound(param);
-  ft_range = st.equal_range(param);
-  std::cout << "lower_bound: " << (it[0] == ite ? "end()" : printPair(it[0], false)) << std::endl;
-  std::cout << "upper_bound: " << (it[1] == ite ? "end()" : printPair(it[1], false)) << std::endl;
-  std::cout << "equal_range: " << (ft_range.first == it[0] && ft_range.second == it[1]) << std::endl;
+	std::cout << "\t-- [" << iter++ << "] --" << std::endl;
+	tmp = st.insert(param);
+	std::cout << "insert return: " << printPair(tmp.first);
+	std::cout << "Created new node: " << tmp.second << std::endl;
+	printSize(st);
 }
 
-template <typename SET>
-void	ft_const_bound(const SET &st, const T1 &param)
+template <typename SET, typename U, typename V>
+void	ft_insert(SET &st, U param, V param2)
 {
-  ft_const_iterator ite = st.end(), it[2];
-  _pair<ft_const_iterator, ft_const_iterator> ft_range;
+	iterator tst;
 
-  std::cout << "\t-- [" << iter++ << "] (const) --" << std::endl;
-  std::cout << "with key [" << param << "]:" << std::endl;
-  it[0] = st.lower_bound(param); it[1] = st.upper_bound(param);
-  ft_range = st.equal_range(param);
-  std::cout << "lower_bound: " << (it[0] == ite ? "end()" : printPair(it[0], false)) << std::endl;
-  std::cout << "upper_bound: " << (it[1] == ite ? "end()" : printPair(it[1], false)) << std::endl;
-  std::cout << "equal_range: " << (ft_range.first == it[0] && ft_range.second == it[1]) << std::endl;
+	std::cout << "\t-- [" << iter++ << "] --" << std::endl;
+	tst = st.insert(param, param2);
+	std::cout << "insert return: " << printPair(tst);
+	printSize(st);
 }
 
 int		main(void)
 {
-  std::list<T1> lst;
-  unsigned int lst_size = 10;
-  for (unsigned int i = 0; i < lst_size; ++i)
-    lst.push_back((i + 1) * 3);
-  ft::set<T1> st(lst.begin(), lst.end());
-  printSize(st);
+	ft::set<T1> st, st2;
 
-  ft_const_bound(st, -10);
-  ft_const_bound(st, 1);
-  ft_const_bound(st, 5);
-  ft_const_bound(st, 10);
-  ft_const_bound(st, 50);
+	ft_insert(st, "lol");
+	ft_insert(st, "mdr");
 
-  printSize(st);
+	ft_insert(st, "mdr");
+	ft_insert(st, "funny");
 
-  ft_bound(st, 5);
-  ft_bound(st, 7);
+	ft_insert(st, "bunny");
+	ft_insert(st, "fizz");
+	ft_insert(st, "buzz");
 
-  printSize(st);
-  return (0);
+	ft_insert(st, st.begin(), "fuzzy");
+
+	ft_insert(st2, st2.begin(), "beauty");
+	ft_insert(st2, st2.end(), "Hello");
+	ft_insert(st2, st2.end(), "World");
+
+	return (0);
 }
